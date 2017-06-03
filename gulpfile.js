7dcac2d94ad9b66c86f4fcb82b2fd3a5d50ba183
Meine Goy <meine@goyman.com>
2017-06-04 00:56:12 +0200

    Add .editorconfig and update gulpfile.js with 2 spaces indentation

--- gulpfile.js
+++ gulpfile.js
@@ -13,83 +13,89 @@ var critical = require('critical');
 // Optimize images
 
 gulp.task('imagemin', function() {
-    return gulp.src(['src/img/*.jpg', 'src/views/images/*.png'], { base: 'src' })
-        .pipe(imagemin(imageminGuetzli()))
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['src/img/*.jpg', 'src/views/images/*.png'], {base: 'src'})
+    .pipe(imagemin(imageminGuetzli()))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('png', function() {
-    return gulp.src('src/img/*.png')
-        .pipe(imagemin(imagemin.optipng({ optimizationLevel: 7 })))
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('src/img/*.png')
+    .pipe(imagemin(imagemin.optipng({optimizationLevel: 7})))
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('responsive-img', function() {
-    return gulp.src('src/views/images/pizzeria.jpg')
-        .pipe(responsive({
-            'pizzeria.jpg': {
-                width: 400,
-                quality: 50
-            }
-        }))
-        .pipe(gulp.dest('dist/views/images'));
+  return gulp.src('src/views/images/pizzeria.jpg')
+    .pipe(responsive({
+      'pizzeria.jpg': {
+        width: 400,
+        quality: 50
+      }
+    }))
+    .pipe(gulp.dest('dist/views/images'));
 });
 
 gulp.task('responsive-thumbnail', function() {
-    return gulp.src('src/views/images/pizzeria.jpg')
-        .pipe(responsive({
-            'pizzeria.jpg': {
-                width: 100
-            }
-        }))
-        .pipe(rename({ suffix: '-thumbnail' }))
-        .pipe(gulp.dest('dist/views/images'));
+  return gulp.src('src/views/images/pizzeria.jpg')
+    .pipe(responsive({
+      'pizzeria.jpg': {
+        width: 100
+      }
+    }))
+    .pipe(rename({
+      suffix: '-thumbnail'
+    }))
+    .pipe(gulp.dest('dist/views/images'));
 });
 
 // Minify html
 
 gulp.task('minify-html', function() {
-    return gulp.src('src/*.html', { base: 'src' })
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/*.html', {base: 'src'})
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 // Clean css
 
 gulp.task('minify-css', function() {
-    return gulp.src('src/css/*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/css/*.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('dist/css'));
 });
 
 // Minify js
 
 gulp.task('uglify', function() {
-    return gulp.src('src/js/*.js', { base: 'src' })
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/js/*.js', {base: 'src'})
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 // Inline critical-path css
 
 gulp.task('critical', ['minify'], function() {
-    critical.generate({
-        inline: true,
-        base: 'dist/',
-        src: 'index.html',
-        css: ['dist/css/style.css'],
-        dest: 'index.html',
-        minify: true,
-        width: 320,
-        height: 480
-    });
+  critical.generate({
+    inline: true,
+    base: 'dist/',
+    src: 'index.html',
+    css: ['dist/css/style.css'],
+    dest: 'index.html',
+    minify: true,
+    width: 320,
+    height: 480
+  });
 });
 
 // Copy `views` except images to `dist`
 
 gulp.task('copy', function() {
-    return gulp.src(['src/views/*.html', 'src/views/css/*', 'src/views/js/*'], { base: 'src/views' })
-        .pipe(gulp.dest('dist/views'));
+  return gulp.src(['src/views/*.html', 'src/views/css/*', 'src/views/js/*'], {base: 'src/views'})
+    .pipe(gulp.dest('dist/views'));
 });
 
 gulp.task('images', ['imagemin', 'png', 'responsive-img', 'responsive-thumbnail']);
@@ -104,7 +110,7 @@ gulp.task('default', ['images', 'critical', 'copy']);
 var del = require('del');
 
 gulp.task('clean', function() {
-    return del([
-        'dist/**/*'
-    ]);
+  return del([
+    'dist/**/*'
+  ]);
 });
